ee47452201f12bbd32d778ad832c872047ad7e69
Claudiu Mihali <claudiumihali@yahoo.com>
2021-05-17 00:52:02 +0300

    Revert "Repo tree experiment"
    This reverts commit 27d5f2d04fe1e106fe8c5d6918dcdb2d80e873c3.
    
    undo experiment

--- part3/phonebook/src/components/Person.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Person = ({name, number, onDelete}) => {
-    return (
-        <div>{name} {number} <button onClick={onDelete}>delete</button></div>
-    )
-}
-
-export default Person
