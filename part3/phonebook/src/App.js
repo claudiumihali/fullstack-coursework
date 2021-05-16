6a637f00b57f19d80b2bf4176f495cc7cfb931d2
Claudiu Mihali <claudiumihali@yahoo.com>
2021-05-17 01:01:57 +0300

    Completed part3

---
+++ part3/phonebook/src/App.js
@@ -0,0 +1,92 @@
+import './App.css'
+
+import React, {useState, useEffect} from 'react'
+
+import * as personsService from './services/Persons'
+
+import Filter from './components/Filter'
+import AddNewPerson from './components/AddNewPerson'
+import Numbers from './components/Numbers'
+import Notification from './components/Notification'
+import Error from './components/Error'
+
+const App = () => {
+  const [notification, setNotification] = useState('')
+  const [error, setError] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personsService.getAll().then((persons) => {setPersons(persons)})
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleAddNewPerson = () => {
+    const newPerson = {name: newName, number: newNumber}
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        personsService.update(existingPerson.id, newPerson).then((updatedPerson) => {
+          setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person))
+          setNewName('')
+          setNewNumber('')
+          setNotification(`Updated the number of ${newPerson.name}`)
+          setTimeout(() => setNotification(''), 5000)
+        }).catch(error => {
+          setError(error.response.data.error)
+          setTimeout(() => setError(''), 5000)
+        })
+      }
+    } else {
+      personsService.create(newPerson).then((newPerson) => {
+        setPersons([...persons, newPerson])
+        setNewName('')
+        setNewNumber('')
+        setNotification(`Added ${newPerson.name}`)
+        setTimeout(() => setNotification(''), 5000)
+      }).catch(error => {
+        setError(error.response.data.error)
+        setTimeout(() => setError(''), 5000)
+      })
+    }
+  }
+
+  const handleDeletePerson = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personsService.deleteById(person.id).then(() => {
+        setPersons(persons.filter(p => p.id !== person.id))
+      }).catch(() => {
+        setError(`Information on ${person.name} has already been removed from the server`)
+        setTimeout(() => setError(''), 5000)
+        setPersons(persons.filter(p => p.id !== person.id))
+      })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={notification} />
+      <Error message={error} />
+      <Filter value={filter} onChange={handleFilterChange} />
+      <AddNewPerson newName={newName} handleNewNameChange={handleNewNameChange} newNumber={newNumber} handleNewNumberChange={handleNewNumberChange}
+        handleAddNewPerson={handleAddNewPerson} />
+      <Numbers filter={filter} persons={persons} onDelete={handleDeletePerson} />
+    </div>
+  )
+}
+
+export default App
---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb://fullstack:${password}@localhost:27017/phonebook_db?retryWrites=true`
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person.save().then(() => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Usage: node mongo.js <password> <name> <number>')
+    mongoose.connection.close()
+}
